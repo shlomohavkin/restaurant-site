bf156b0efe10edc082ace4d9a495527e82d67557
Shlomo Havkin <Shlomo@Shlomos-MacBook-Pro.local>
2020-10-04 23:04:17 +0300

    adding javascript to the site

---
+++ javascript/javascript.js
@@ -0,0 +1,20 @@
+// booking form and inputs
+let bookingForm = document.getElementById('booking');
+let nameInput = document.getElementById('name-input');
+let peopleInput = document.getElementById('people-input');
+let phoneInput = document.getElementById('phone-input');
+let timeInput = document.getElementById('time-input');
+let submitInput = document.getElementById('book-button');
+
+
+
+
+
+
+
+
+
+
+
+
+
