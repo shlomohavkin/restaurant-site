175929488cbc032b30678caf334180082eb12fa5
Shlomo Havkin <Shlomo@Shlomos-MacBook-Pro.local>
2020-10-08 20:49:46 +0300

    I sent the values of the inputs in the booking form to the console.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,20 +1,24 @@
-// booking form and inputs
-let bookingForm = document.getElementById('booking');
-let nameInput = document.getElementById('name-input');
-let peopleInput = document.getElementById('people-input');
-let phoneInput = document.getElementById('phone-input');
-let timeInput = document.getElementById('time-input');
-let submitInput = document.getElementById('book-button');
+document.addEventListener("DOMContentLoaded", function () {
+    // booking inputs
+    let nameInput = document.getElementById('name-input');
+    let peopleInput = document.getElementById('people-input');
+    let phoneInput = document.getElementById('phone-input');
+    let timeInput = document.getElementById('time-input');
+    let submitInput = document.getElementById('book-button');
 
+    // booking form
+    let bookingForm = document.getElementById('booking');
 
+    function logSubmit(event) {
+        const bookingBody = {
+            name: nameInput.value,
+            people: peopleInput.value,
+            phone: phoneInput.value,
+            time: timeInput.value
+        }
+    
+        console.log(JSON.stringify(bookingBody));
+    }
 
-
-
-
-
-
-
-
-
-
-
+    bookingForm.addEventListener("submit", logSubmit);
+});
